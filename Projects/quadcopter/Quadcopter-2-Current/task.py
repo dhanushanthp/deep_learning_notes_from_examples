3d2e3fd4626adcaf679e47a8a899152ba3ba6823
Dhanushanth Pushparajah <dhanushanthp@Dhanushanths-MacBook-Pro.local>
2018-04-23 09:26:32 +0800

    Adding...

--- Projects/quadcopter/Quadcopter-2-Current/task.py
+++ Projects/quadcopter/Quadcopter-2-Current/task.py
@@ -29,6 +29,7 @@ class Task():
     def get_reward(self):
         """Uses current pose of sim to return reward."""
         reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        # reward = -min(abs(self.sim.pose[2] - self.target_pos[2]), 10.0)
         return reward
 
     def step(self, rotor_speeds):
